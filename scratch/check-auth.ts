86e9d721290d8afd77d9b3e3ed51dbbc8b947c8a
rezmi-20 <rezmi-20@users.noreply.github.com>
2026-05-10 15:37:04 +0300

    fix: remove broken scratch files and finalize type safety for build

--- scratch/check-auth.ts
+++
@@ -1,6 +0,0 @@
-import { auth } from "./lib/auth/server";
-
-console.log("Auth keys:", Object.keys(auth));
-if (auth.handlers) {
-  console.log("Auth handlers keys:", Object.keys(auth.handlers));
-}
